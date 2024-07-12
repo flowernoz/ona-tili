f1383e4240adf186882e1d9b7fa1faaf1f6af5bc
Flowernoza <cadillac2111@Gulnozas-Mac.local>
2024-07-12 19:33:28 +0500

    first

--- src/components/features/Features.js
+++ src/components/features/Features.js
@@ -2,7 +2,7 @@ import React from "react";
 import { GiNotebook } from "react-icons/gi";
 import { HiPencilAlt } from "react-icons/hi";
 import { MdOutlineMenuBook } from "react-icons/md";
-
+import { NavLink } from "react-router-dom";
 import Card from "./Card";
 
 const Features = () => {
@@ -12,15 +12,27 @@ const Features = () => {
       className="w-full h-[100vh] px-12 py-16 border-b-[1px] border-b-black"
     >
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 xl:gap-14">
-        <Card title="Diktantlar" icon={<GiNotebook />} />
-        <Card title="Esselar" icon={<GiNotebook />} />
-        <Card title="Bayonlar" icon={<GiNotebook />} />
-        <Card title="Baholash me'zoni" icon={<HiPencilAlt />} />
-        <Card
-          title="Yozilishi qiyin so'zlar imlosi"
-          icon={<MdOutlineMenuBook />}
-        />
-        <Card title="Ayrim so'zlar izohi" icon={<MdOutlineMenuBook />} />
+        <NavLink>
+          <Card title="Diktantlar" icon={<GiNotebook />} />
+        </NavLink>
+        <NavLink>
+          <Card title="Esselar" icon={<GiNotebook />} />
+        </NavLink>
+        <NavLink>
+          <Card title="Bayonlar" icon={<GiNotebook />} />
+        </NavLink>
+        <NavLink>
+          <Card title="Baholash me'zoni" icon={<HiPencilAlt />} />
+        </NavLink>
+        <NavLink>
+          <Card
+            title="Yozilishi qiyin so'zlar imlosi"
+            icon={<MdOutlineMenuBook />}
+          />
+        </NavLink>
+        <NavLink>
+          <Card title="Ayrim so'zlar izohi" icon={<MdOutlineMenuBook />} />
+        </NavLink>
       </div>
     </section>
   );
